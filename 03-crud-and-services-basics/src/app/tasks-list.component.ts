6024d3322327712c942789b940e474ed23683a91
Adrian Domin <domin._.adrian@wp.pl>
2023-12-06 21:01:52 +0100

    Add delete, update and new service to hold this functions

--- 03-crud-and-services-basics/src/app/tasks-list.component.ts
+++ 03-crud-and-services-basics/src/app/tasks-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { Component, Inject, Input, inject } from "@angular/core";
 import { Task } from "./Task";
 import { NgFor, NgIf } from "@angular/common";
 import { NgIconComponent, provideIcons } from "@ng-icons/core";
 import { featherCalendar } from "@ng-icons/feather-icons";
 import { RemoveItemButtonComponent } from "./remove-item-button.component";
 import { AutosizeTextareaComponent } from "./autosize-textarea.component";
+import { TasksService } from "./tasks.service";
 
 @Component({
   selector: "app-tasks-list",
@@ -20,19 +21,23 @@ import { AutosizeTextareaComponent } from "./autosize-textarea.component";
   template: `
     <ul>
       <li *ngFor="let task of tasks" class="mb-2">
-        <div class="rounded-md shadow-md p-4 block" [class.bg-green-300]="task.done">
+        <div
+          class="rounded-md shadow-md p-4 block"
+          [class.bg-green-300]="task.done"
+        >
           <button
             class="w-full"
             (click)="handleSingleClick(task)"
             (dblclick)="switchToEditMode()"
           >
             <header class="flex justify-end">
-              <app-remove-item-button />
+              <app-remove-item-button (confirm)="delete(task.id)" />
             </header>
             <section class="text-left">
               <app-autosize-textarea
                 *ngIf="editMode; else previewModeTemplate"
                 (keyup.escape)="editMode = false"
+                (submitText)="updateTask(task.id, $event)"
                 [value]="task.name"
               />
 
@@ -59,6 +64,16 @@ export class TasksListComponent {
   editMode = false;
 
   isSingleClick = true;
+
+  private tasksService = inject(TasksService);
+
+  delete(taskId: number) {
+    this.tasksService.delete(taskId);
+  }
+
+  updateTask(taskId: number, name: string) {
+    this.tasksService.update(taskId, name);
+  }
 
   handleSingleClick(task: Task) {
     this.isSingleClick = true;
